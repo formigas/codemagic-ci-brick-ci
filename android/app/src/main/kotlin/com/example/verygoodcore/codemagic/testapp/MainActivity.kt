d8af52c0b52d019604d17e75c9885a92e768243c
Peter Trost <p.trost93@gmail.com>
2024-09-19 08:14:21 +0200

    chore: Add test app

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/codemagic/testapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.codemagic_testapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:codemagic_testapp/app/app.dart';
+import 'package:codemagic_testapp/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
